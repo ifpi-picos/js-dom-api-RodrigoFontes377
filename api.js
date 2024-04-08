6de4d48f6d53f7c56642847e548b45405fe852ff
Rodrigo Fontes <rodrigofontes377@gmail.com>
2024-04-08 00:13:37 -0300

    finalizado

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-document.getElementById("buscar-btn").addEventListener("click", async () => {
+async function buscarRepositorios() {
   const username = document.getElementById("input-git").value.trim();
   const response = await fetch(
     `https://api.github.com/users/${username}/repos`
@@ -11,7 +11,21 @@ document.getElementById("buscar-btn").addEventListener("click", async () => {
   if (data.message === "Not Found") {
     resultadoDiv.innerHTML = "Usuário não encontrado";
   } else {
-    const repositorios = data.map((repo) => `<p>${repo.full_name}</p>`);
-    resultadoDiv.innerHTML = repositorios.join("");
+    const listaRepositorios = document.createElement("ul");
+    listaRepositorios.classList.add("lista-repos-dinamica");
+    data.forEach((repo) => {
+      const listItem = document.createElement("li");
+      const link = document.createElement("a");
+      link.href = repo.html_url;
+      link.textContent = repo.full_name;
+      link.target = "_blank"; // Abre o link em uma nova aba
+      listItem.appendChild(link);
+      listaRepositorios.appendChild(listItem);
+    });
+    resultadoDiv.appendChild(listaRepositorios);
   }
-});
+}
+
+document
+  .getElementById("buscar-btn")
+  .addEventListener("click", buscarRepositorios);
